b1665b9f5a4cdeddd46d4f16117c3a44b4ea02b0
AzatG <>
2021-05-03 15:37:54 +0000

    3.1.1a

--- poisson.cc
+++ poisson.cc
@@ -4,17 +4,23 @@
 
 
 double poisson(double mu, int k) {
-    return 0;
+  return 0;
 }
 
 int main() {
     using namespace std;
-
+    
+    vector<int> zaehler(11);
 
     ifstream fin("datensumme.txt");
     int n_i;
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        zaehler[n_i] += 1;
+    }
+    for (unsigned  int k = 0 ; k < zaehler.size() ; ++k)   
+    { 
+      std::cout  << k << ":" << zaehler[k] << std::endl;
     }
     fin.close();
 }
